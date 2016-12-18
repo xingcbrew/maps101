57a3a2d815c4c1d1abd376d5216a9937eb9cc295
Xing C. Brew <xing@Xings-MacBook-Air.local>
2016-12-18 18:41:07 -0500

    world languages map

---
+++ worldmap.R
@@ -0,0 +1,68 @@
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(ggmap)
+library(rgdal)
+library(rgeos)
+library(maptools)
+library(tidyr)
+library(tmap)
+library(raster) #to read in shapefile
+library(sp)
+library(leaflet)
+
+#set working directory
+setwd("/Users/xing/Documents/maps101")
+
+# import data (strip.white removes leading and trailing whitespace)
+dat1 <- read.csv("/Users/xing/Documents/maps101/data.csv", strip.white=TRUE)
+
+# clean data
+dat1 <- subset(dat1, select = c("Name.in.English", "Countries", "Country.codes.alpha.3", "Degree.of.endangerment",
+                        "Number.of.speakers", "Latitude", "Longitude"))
+
+names(dat1) <- c("language", "country", "country_code", "degree_endanger", "speakers", "lat", "long")
+
+# where number of speakers is 0, make degree endangered 'extinct'
+dat1$degree_endanger[dat1$speakers ==0] <- 'Extinct'
+
+# disable scientific notation
+options(scipen = 999)
+
+# extract data on canada
+namer <- dat1[dat1$country_code == "CAN" | dat1$country_code == "CAN, USA" | dat1$country_code == "USA" |
+                dat1$country_code == "MEX",]
+
+### using leaflet
+
+# relevel factors so that colors match and legend ordered correctly
+namer$degree_endanger <- factor(namer$degree_endanger, levels = c("Vulnerable", 
+                                                                  "Definitely endangered", "Critically endangered",
+                                                                  "Severely endangered", "Extinct"))
+# make colour palette
+pal <- colorFactor(c("#FFC300", "#FF5733", "#C70039", "#900C3F", "#808B96"), namer$degree_endanger)
+
+# pop-up information
+lab <- paste(namer$language,"|", "Speakers:", namer$speakers, sep = " ")
+
+# make the map
+m <- leaflet(data = namer) %>% setView(lng = -79.38318, lat= 43.65323, zoom = 3)
+
+lang_map <- m %>% addProviderTiles("CartoDB.Positron", options = providerTileOptions(opacity = 0.35)) %>%
+  addCircleMarkers(~long, ~lat, popup = ~as.character(lab),
+                   radius = 5,
+                   stroke = FALSE, fillOpacity = 0.5,
+                   color = ~pal(degree_endanger)) %>%
+  addLegend("topright", pal = pal, values = ~degree_endanger,
+            title = "Degree Endangered", opacity = 0.6)
+
+
+#### load world map using ggplot2  ###
+# worldmap <- borders("world", colour="gray50", fill="gray") # create a layer of borders
+# 
+# ggplot() + worldmap +
+#   geom_point(data = nafta, 
+#              aes(x = long, y = lat,
+#                  color = degree_endanger)) +
+#   theme_nothing(legend = TRUE)
+
